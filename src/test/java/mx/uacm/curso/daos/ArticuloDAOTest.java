54246df743f7441588fe0cb188569868e13147bf
Ismael Robles <iarobles271@gmail.com>
2020-02-28 19:15:05 -0600

    ejemplo testing

--- src/test/java/mx/uacm/curso/daos/ArticuloDAOTest.java
+++ src/test/java/mx/uacm/curso/daos/ArticuloDAOTest.java
@@ -5,6 +5,7 @@
  */
 package mx.uacm.curso.daos;
 
+import java.util.Date;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -39,6 +40,30 @@ public class ArticuloDAOTest {
         Articulo a = articuloDAO.buscarPorId(1);
         //checamos que la variable a no sea NULL
         Assertions.assertNotNull(a);
+        //revisamos que el id del objeto sea el correcto
+        //primer argumento = el valor esperado
+        //segundo argumento = el valor real
+        Assertions.assertEquals(1,a.getId());
+    }
+    
+    @Test
+    public void guardarArticuloTest(){
+        
+        Articulo a = new Articulo();
+        a.setContenido("contenido test");
+        a.setTitulo("titulo test");
+        a.setFechaCreacion(new Date());
+        
+        Articulo a2 = articuloDAO.guardar(a);
+        //verificamos que el articulo se guardo
+        //revisamos que no sea null
+        Assertions.assertNotNull(a2);
+        //revisamos que tenga un id
+        Assertions.assertNotNull(a2.getId());
+        //revisamos que el contenido se guardo bien
+        Assertions.assertEquals(a.getContenido(),a2.getContenido());
+        Assertions.assertEquals(a.getTitulo(),a2.getTitulo());
+        Assertions.assertEquals(a.getFechaCreacion(),a2.getFechaCreacion());
     }
 
 }
