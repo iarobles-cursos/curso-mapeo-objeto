7c90138ba99cc97a1f75930c8dfceae062bac5c7
Ismael Robles <iarobles271@gmail.com>
2020-05-05 16:50:53 -0500

    Clase Martes 5 de Mayo de 2020: ejemplo 4, multiples propiedades de una entidad

--- src/main/java/mx/uacm/curso/EjemploCriterios.java
+++ src/main/java/mx/uacm/curso/EjemploCriterios.java
@@ -11,6 +11,8 @@ import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import mx.uacm.curso.dtos.ArticuloDTO;
+import mx.uacm.curso.entidades.Articulo;
 import mx.uacm.curso.entidades.Venta;
 
 /**
@@ -47,12 +49,22 @@ public class EjemploCriterios {
         CriteriaQuery<String> criterio2 = constructor.createQuery(String.class);
         Root<Venta> v2 = criterio2.from(Venta.class);
         criterio2.select(v2.<String>get("nombreProducto"));
-        
+
         TypedQuery<String> consulta2 = em.createQuery(criterio2);
         List<String> nombreProductos = consulta2.getResultList();
         System.out.println("productos:" + nombreProductos);
-                
+
+        //EJEMPLO 3
+        //SELECT new com.proyecto.ArticuloDTO(a.id, a.titulo) FROM Articulo a 
+        CriteriaQuery<ArticuloDTO> criterio3 = constructor.createQuery(ArticuloDTO.class);
+        Root<Articulo> a = criterio3.from(Articulo.class);
+        criterio3.select(constructor.construct(ArticuloDTO.class,a.get("id"),a.get("titulo")));
         
+        TypedQuery<ArticuloDTO> consulta3 = em.createQuery(criterio3);
+        List<ArticuloDTO> articulosdtos = consulta3.getResultList();
+        for(ArticuloDTO ad : articulosdtos){
+            System.out.println("titulo:" + ad.getTitulo());
+        }
 
     }
 
