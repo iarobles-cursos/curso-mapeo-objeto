ef98565aedd673a008e89ef2de8d15bdd590f317
Ismael Robles <iarobles271@gmail.com>
2020-05-05 15:30:09 -0500

    Clase Martes 5 de Mayo de 2020: ejemplos de introducción a criterios de JPA

--- src/main/java/mx/uacm/curso/EjemploCriterios.java
+++ src/main/java/mx/uacm/curso/EjemploCriterios.java
@@ -8,6 +8,10 @@ package mx.uacm.curso;
 import java.io.*;
 import java.util.*;
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import mx.uacm.curso.entidades.Venta;
 
 /**
  *
@@ -18,8 +22,23 @@ public class EjemploCriterios {
     public static void main(String[] args) throws IOException {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("blog-pruebas-memoria");
         EntityManager em = emf.createEntityManager();
-        
-        
+
+        //SELECT v FROM Venta v WHERE v.id.orden=1
+        boolean incluirWhere = false;
+        CriteriaBuilder constructor = em.getCriteriaBuilder();
+        CriteriaQuery<Venta> criterio = constructor.createQuery(Venta.class);
+        Root<Venta> v = criterio.from(Venta.class);
+        criterio.select(v);
+        if (incluirWhere) {
+            criterio.where(constructor.equal(v.get("id").get("orden"), 1));
+        }
+
+        TypedQuery<Venta> consulta = em.createQuery(criterio);
+        List<Venta> ventas = consulta.getResultList();
+
+        for (Venta venta : ventas) {
+            System.out.println("producto:" + venta.getNombreProducto());
+        }
 
     }
 
